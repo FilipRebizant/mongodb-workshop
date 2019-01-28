1add66f251cd88c7236c5f140a20f58c774f20e6
Tomasz Tarnawski <tarnawski27@gmail.com>
2019-01-28 22:38:32 +0100

    Query 3.0

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 $collection = (new MongoDB\Client('mongodb://mongodb/'))->test->books;
 
-$results = $collection->distinct('status');
+$results = $collection->aggregate([
 
-var_dump($results);
+    ['$group' => ['_id' => '$status', 'count' => ['$sum' => 1]]],
+    ['$sort' => ['count' => -1]],
+
+]);
+
+var_dump($results->toArray());
