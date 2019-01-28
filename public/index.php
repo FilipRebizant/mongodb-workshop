c049cb4b79e43986986dc7b7d9296b8913561ff2
Tomasz Tarnawski <tarnawski27@gmail.com>
2019-01-28 22:21:14 +0100

    Query 2.0

--- public/index.php
+++ public/index.php
@@ -4,10 +4,6 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 $collection = (new MongoDB\Client('mongodb://mongodb/'))->test->books;
 
-$results = $collection->find([
-    'title' => [
-        '$regex' => '.*PHP.*'
-    ]
-]);
+$results = $collection->distinct('status');
 
-var_dump($results->toArray());
+var_dump($results);
