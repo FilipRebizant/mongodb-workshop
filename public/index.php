f71c8a9f45e1e618da81a8c8d64eda2842d0b6b4
Tomasz Tarnawski <tarnawski27@gmail.com>
2019-01-28 21:32:25 +0100

    Load data to mongodb

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-phpinfo();
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$data = file_get_contents(__DIR__ . '/../data/books.json');
+$books = json_decode($data, true);
+
+$collection = (new MongoDB\Client('mongodb://mongodb/'))->test->books;
+$insertManyResult = $collection->insertMany($books);
+
+printf("Inserted %d document(s)\n", $insertManyResult->getInsertedCount());
