5d3559b881916e72f2a4d167b49cef01ab780ffe
Tomasz Tarnawski <tarnawski27@gmail.com>
2019-01-28 22:11:50 +0100

    Query 1.0

--- public/index.php
+++ public/index.php
@@ -2,10 +2,12 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$data = file_get_contents(__DIR__ . '/../data/books.json');
-$books = json_decode($data, true);
-
 $collection = (new MongoDB\Client('mongodb://mongodb/'))->test->books;
-$insertManyResult = $collection->insertMany($books);
 
-printf("Inserted %d document(s)\n", $insertManyResult->getInsertedCount());
+$results = $collection->find([
+    'title' => [
+        '$regex' => '.*PHP.*'
+    ]
+]);
+
+var_dump($results->toArray());
