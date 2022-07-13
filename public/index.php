20f50ac427546e1dfe535616d3e1e35a8f3c74b7
Filip Rebizant (PGS Software) <frebizant@pgs-soft.com>
2022-07-13 09:22:16 +0200

    Revert "Mongo add some queries"
    This reverts commit d46c8db898eef033fa397ee756f2f30777c9cea0.

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-header('Content-type: application/json');
-
 $client = new MongoDB\Client('mongodb://mongodb/');
 $collection = $client->playground->books;
 
@@ -14,10 +12,11 @@ $collection = $client->playground->books;
 //$json = json_decode($string, true);
 //$collection->insertMany($json);
 
+
 $specificTitleWithLike = $collection->find([
     'title' => ['$regex' => 'Gnuplot in Act']
 ]);
-//echo json_encode($specificTitleWithLike->toArray());
+//print_r($specificTitleWithLike->toArray());
 
 $onlySpecificFields = $collection->aggregate([
     ['$project' =>
@@ -29,18 +28,24 @@ $onlySpecificFields = $collection->aggregate([
     ],
     ['$match' => ['pageCount' => ['$gt' => 800]]], // Get books with more than 800 pages
     ['$limit' => 5],
-    ['$skip' => 2], // Skip two records (already from limited 5)
+    ['$skip' => 2], // Skip first two records
     ['$sort' => ['pageCount' => -1]]
 ]);
 
-//echo json_encode($onlySpecificFields->toArray());
+//print_r($onlySpecificFields);
 
+
+//    ['$group' => ['_id' => '$status', 'count' => ['$sum' => 1]]],
+//    ['$sort' => ['count' => -1]],
+
+$startDate = strtotime("2008-01-01 00:00:00");
+$endDate = strtotime("2010-12-31 00:00:00");
 $booksPublishedInSpecificYear = $collection->aggregate([
     ['$match' => [
         "publishedDate" =>
             [
-                '$gt' => '2010-01-01',
-                '$lte' => '2010-12-31',
+                '$gt' => new MongoDB\BSON\UTCDateTime($startDate),
+                '$lte' => new MongoDB\BSON\UTCDateTime($endDate)
             ]
     ]],
     ['$project' =>
@@ -50,11 +55,18 @@ $booksPublishedInSpecificYear = $collection->aggregate([
             'publishedDate' => 1,
         ]
     ],
-    ['$sort' => ['publishedDate' => 1]]
 ]);
-//echo json_encode($booksPublishedInSpecificYear->toArray());
 
-$differentThanPublished = $collection->aggregate([
+//var_dump($booksPublishedInSpecificYear->toArray());
+
+
+$res = $collection->aggregate([
+  ['$project' => ['title' => 1, "publishedDate" => 1]],
+  ['$match' =>  ['publishedDate' => new MongoDB\BSON\UTCDateTime(strtotime('2010-07-01'))]]
+]);
+print_r($res->toArray());
+
+$notPublished = $collection->aggregate([
     ['$project' =>
         [
             '_id' => 0,
@@ -66,70 +78,7 @@ $differentThanPublished = $collection->aggregate([
     ['$match' => ['status' => ['$ne' => 'PUBLISH']]],
 ]);
 
-//echo json_encode($differentThanPublished->toArray());
+//var_dump($notPublished->toArray());
 
 $categories = $collection->distinct("categories");
-//json_encode($categories);
-
-$numberOfBooksAboutJava = $collection->aggregate([
-    ['$match' => ['shortDescription' => ['$regex' => 'Java']]],
-    ['$group' => ['_id' => null, 'count' => ['$sum' => 1]]],
-]);
-//echo json_encode($numberOfBooksAboutJava->toArray());
-
-$numberOfBooksInStatuses = $collection->aggregate([
-    ['$group' => ['_id' => '$status', 'count' => ['$sum' => 1]]],
-    ['$sort' => ['count' => -1]],
-]);
-
-//echo json_encode($numberOfBooksInStatuses->toArray());
-
-$booksByAuthor = $collection->aggregate([
-    ['$unwind' => '$authors'],
-//    ['$match' => ['authors' => 'Andrew Schmidt']],
-    [
-        '$group' =>
-            [
-                '_id' => [
-                    '$title',
-                    '$authors',
-                ],
-                'author' => ['$first' => '$authors'],
-                'title' => ['$first' => '$title']
-            ]
-    ],
-    [
-        '$group' =>
-            [
-                '_id' => ['$author'],
-                'author' => ['$first' => '$author'],
-                'title' => ['$push' => '$title']
-            ]
-    ],
-
-    ['$project' => [
-        '_id' => 0,
-        'result' => [
-            'author' => '$author',
-            'titles' => '$title',
-        ],
-    ]],
-//    ['$out' => 'booksPerAuthor'] // - Put result into new collection
-]);
-
-$result = $client->playground->booksPerAuthor->find();
-//echo json_encode($result->toArray());
-//echo json_encode($booksByAuthor->toArray());
-
-$numberOfBooksPerCategory = $collection->aggregate([
-    ['$unwind' => '$categories'],
-    ['$project' => [
-        'categories' => ['$toLower' => '$categories']] // Project to treat as same eg. Java with java
-    ],
-    ['$group' => [
-        '_id' => ['$categories'],
-        'counter' => ['$sum' => 1]
-    ]],
-]);
-
-echo json_encode($numberOfBooksPerCategory->toArray());
+//var_dump($categories);
